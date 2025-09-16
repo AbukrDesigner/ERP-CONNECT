35002a89b53709474b743d8ff6a1dfec7b28856f
AbukrDesigner <boubacartourebalde2@gmail.com>
2025-09-16 11:25:17 +0200

    Ajout de code

--- src/app/back-office/composants/login/login.ts
+++ src/app/back-office/composants/login/login.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ FormsModule],
+  standalone: true,
+  imports: [ FormsModule, ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  constructor(private router : Router) {}
+     email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+
+  onSubmit() {
+    console.log('Form submitted');
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Remember Me:', this.rememberMe);
+    
+    // ici tu peux appeler ton service pour authentification
+  }
 
 }
